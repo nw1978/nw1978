58c116d1d1d4394b56dad8f5ce9f150ca15d4ab7
nw1978 <nw1978@gmail.com>
2024-05-08 23:37:32 -0400

    s

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    const scene = new THREE.Scene();
+    const scene = new three.Scene();
     
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({color: "FF0000"}); 
-    const cube = new THREE.Mesh(geometry, material);
+    const geometry = new three.BoxGeometry(1, 1, 1);
+    const material = new three.MeshBasicMaterial({color: "FF0000"}); 
+    const cube = new three.Mesh(geometry, material);
 
     scene.add(cube);
     cube.position.set(0, 0, -2);
     cube.rotation.set(0, Math.PI/4, 0);
 
-    const camera = new THREE.PerspectiveCamera();
+    const camera = new three.PerspectiveCamera();
     camera.position.set(1, 1, 5);
 
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new three.WebGLRenderer();
     renderer.setSize(500, 500); 
     renderer.render(scene, camera);
 
