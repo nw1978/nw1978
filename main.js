db1f418be71e830dd6c422147f942363d28833a5
nw1978 <nw1978@gmail.com>
2024-05-08 23:15:39 -0400

    s

--- main.js
+++ main.js
@@ -8,4 +8,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const material = new THREE.MeshBasicMaterial({color: "FF0000"}); 
     const cube = new THREE.Mesh(geometry, material);
 
-    })
+    scene.add(cube);
+    cube.position.set(0, 0, -2);
+    cube.rotation.set(0, Math.PI/4, 0);
+
+    const camera = new THREE.PerspectiveCamera();
+    camera.position.set(1, 1, 5);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(500, 500); 
+    renderer.render(scene, camera);
+
+    document.body.appendChild(renderer.domElement);
+    });
