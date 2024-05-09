42a6e371916efeec1315f3729015190fc122a6f1
nw1978 <nw1978@gmail.com>
2024-05-08 23:22:39 -0400

    s

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@v0.164.1/build/three.module.js';
-import * as THREE_ADDONS from 'https://cdn.jsdelivr.net/npm/three@v0.164.1/examples/jsm/';
 
 document.addEventListener("DOMContentLoaded", () => {
     const scene = new THREE.Scene();
