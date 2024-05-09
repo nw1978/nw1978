650aeaa1dcb33b6a231631f7f17f77d2c0648037
nw1978 <nw1978@gmail.com>
2024-05-08 22:29:46 -0400

    n

---
+++ main.js
@@ -0,0 +1,4 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@v0.164.1/build/three.module.js';
+//import * as THREE_ADDONS from 'https://cdn.jsdelivr.net/npm/three@v0.164.1/examples/jsm/';
+
+console.log(THREE);
