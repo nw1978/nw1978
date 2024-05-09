b729289d6e9c1ff3b8d307755c2b2c1e8668e83c
nw1978 <nw1978@gmail.com>
2024-05-08 23:06:22 -0400

    c

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@v0.164.1/build/three.module.js';
 import * as THREE_ADDONS from 'https://cdn.jsdelivr.net/npm/three@v0.164.1/examples/jsm/';
 
+document.addEventListener("DOMContentLoaded", () => {
+    const scene = new THREE.Scene();
+    
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({color: "FF0000"}); 
+    const cube = new THREE.Mesh(geometry, material);
 
-console.log(THREE);
+    )
